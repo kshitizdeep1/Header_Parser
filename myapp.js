c714d8ace4e39b1e5949f46ff0de752c8b728ee3
Glitch (flicker-curly-heaven) <none>
2020-05-02 13:08:45 +0000

    🍏🐃 Checkpoint ./myapp.js:15705542/153 ./server.js:15705542/68

--- myapp.js
+++ myapp.js
@@ -1,4 +1,9 @@
 
+var express = require('express');
+var app = express();
+
 app.get("/api/whoami",(req,res)=>{
   res.json({ipaddress:req.connection.remoteAddress,language:req.connection.language});
 });
+
+module.exports=app;
